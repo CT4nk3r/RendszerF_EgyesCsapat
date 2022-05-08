f693f4cbdba88dac66f00a37b12f291d37341a41
BraghiniBenjamin <60982244+BraghiniBenjamin@users.noreply.github.com>
2022-05-08 14:41:03 +0200

    Update general.js

--- RendszerF_1escsapat-main/backend/controllers/general.js
+++ RendszerF_1escsapat-main/backend/controllers/general.js
@@ -1,9 +1,51 @@
+import Maintenance from "../database/models/maintenance.js";
+import Repairer from "../database/models/repairer.js";
+import formatDate from '../utilities/format-date.js';
 
 const controller = {};
 
+Date.prototype.addDays = function (days) {
+    let date = new Date(this.valueOf());
+    date.setDate(date.getDate() + days);
+    return date;
+  }
+
 controller.getIndex = async function (req, res, next) {
     try {
-        res.send({ message: 'Success!' });
+        if(req.cookies.token == req.sessionID) {
+            const maintenances = await Repairer.findAll({
+                where: {
+                    id: req.user.id    
+                },
+                include: {model: Maintenance,raw:true}
+            })
+
+
+            let mc = [];
+
+            let date = 0; 
+
+            for(let main of maintenances){
+                let newDate = formatDate(main.maintenance.lastInstance)
+                newDate = new Date(newDate)
+                newDate = newDate.addDays(main.maintenance.period)
+                let currentDate = new Date()
+                if(main.maintenance.reoccuring == true){
+                    while (newDate < currentDate){
+                        newDate = newDate.addDays(main.maintenance.period)                
+                    }
+                }
+                mc.push({
+                    desc: main.maintenance.desc,
+                    locationId: main.maintenance.locationId,
+                    time: formatDate(newDate)  
+                })
+            }
+            res.render('yourpage.ejs', {data : mc})
+        }
+        else{
+            res.render('login.ejs')
+        }
     } catch (error) {
         console.error(error);
         res.send(JSON.stringify({ error: error.message }));
